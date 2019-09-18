dee44ac829137acea3414a1a515b097e693e3be5
Vuzeta95 <machnio95@o2.pl>
2019-09-18 19:48:50 +0200

    first commit

---
+++ src/root/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.scss';
+
+const app = () => {
+  return (
+    <div class="container">
+      <div class="sideNav">
+        <h1>Box 1</h1>
+      </div>
+      <div class="upperNav">
+        <h1>Box 2</h1>
+      </div>
+      <div class="centerNav">
+        <h1>Box 3</h1>
+      </div>
+    </div>
+  );
+};
+
+export default app;
