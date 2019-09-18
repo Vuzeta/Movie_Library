6101b1713d5c5d32a3dc3011094c9230d564ee54
Vuzeta95 <machnio95@o2.pl>
2019-09-18 21:35:13 +0200

    add style sidenav, scrollbar

--- src/layouts/SideNav/SideNav.js
+++ src/layouts/SideNav/SideNav.js
@@ -4,7 +4,42 @@ import './SideNav.scss';
 const SideNav = () => {
   return (
     <div class="sideNav">
-      <h1>Box 1</h1>
+      <div className="sideNav__wrapper">
+        <div className="logoWrapper">
+          <p className="logoWrapper__logo">Movie Library</p>
+        </div>
+        <div className="sideNav__mainLinksWrapper">
+          <ul className="mainLinks">
+            <li className="mainLinks__link sideNav__link">Popularity</li>
+            <li className="mainLinks__link sideNav__link">TV Shows</li>
+            <li className="mainLinks__link sideNav__link">Favourite</li>
+          </ul>
+        </div>
+        <div className="categoriesWrapper">
+          <p className="categoriesWrapper__title">Categories</p>
+          <ul className="categories">
+            <li className="sideNav__link">Action</li>
+            <li className="sideNav__link">Adventure</li>
+            <li className="sideNav__link">Animation</li>
+            <li className="sideNav__link">Comedy</li>
+            <li className="sideNav__link">Crime</li>
+            <li className="sideNav__link">Documentary</li>
+            <li className="sideNav__link">Drama</li>
+            <li className="sideNav__link">Family</li>
+            <li className="sideNav__link">Fantasy</li>
+            <li className="sideNav__link">History</li>
+            <li className="sideNav__link">Horror</li>
+            <li className="sideNav__link">Music</li>
+            <li className="sideNav__link">Mystery</li>
+            <li className="sideNav__link">Romance</li>
+            <li className="sideNav__link">Science Fiction</li>
+            <li className="sideNav__link">TV Movie</li>
+            <li className="sideNav__link">Thriller</li>
+            <li className="sideNav__link">War</li>
+            <li className="sideNav__link">Western</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
