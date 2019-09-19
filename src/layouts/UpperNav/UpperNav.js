39a7589e28ef0ad6d03512ddd1af63a49c6a9f9a
Vuzeta <machniik95@gmail.com>
2019-09-19 14:31:54 +0200

    add upperNav

--- src/layouts/UpperNav/UpperNav.js
+++ src/layouts/UpperNav/UpperNav.js
@@ -1,12 +1,62 @@
 import React from 'react';
 import './UpperNav.scss';
 
+import homepageIcon from '../../assets/homepage_logo.svg';
+import facebookIcon from '../../assets/facebook_logo.svg';
+import linkedinIcon from '../../assets/linkedin_logo.svg';
+import githubIcon from '../../assets/github_logo.svg';
+
 const UpperNav = () => {
-  return (
-    <div class="upperNav">
-      <h1>Box 2</h1>
-    </div>
-  );
+	return (
+		<div class="upperNav">
+			<div class="upperNav__wrapper">
+				<div className="navigation">
+					<span className="navigation__left">&#x0003C;</span>
+					<span className="navigation__center">&#x000B7;</span>
+					<span className="navigation__right">&#x0003E;</span>
+				</div>
+				<div class="search">
+					<div>
+						<input type="text" placeholder="     Search movie" required />
+					</div>
+				</div>
+				<div className="socialMedia">
+					<a href="https://vuzeta.github.io/Portfolio/" target="_blank" rel="noopener noreferrer">
+						<img
+							src={homepageIcon}
+							alt="homepage logo"
+							className="socialMedia__logo upperNav__logo"
+						/>
+					</a>
+					<a
+						href="https://www.facebook.com/mateusz.machnik.1?ref=bookmarks"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<img
+							src={facebookIcon}
+							alt="facebook logo"
+							className="socialMedia__logo upperNav__logo"
+						/>
+					</a>
+					<a
+						href="https://www.linkedin.com/in/mateusz-machnik-566468159/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<img
+							src={linkedinIcon}
+							alt="linkedin logo"
+							className="socialMedia__logo upperNav__logo"
+						/>
+					</a>
+					<a href="https://github.com/Vuzeta" target="_blank" rel="noopener noreferrer">
+						<img src={githubIcon} alt="github logo" className="socialMedia__logo upperNav__logo" />
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default UpperNav;
