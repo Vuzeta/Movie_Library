c6f3625cd724a91797f9609838861295a3beb3c5
Vuzeta95 <machnio95@o2.pl>
2019-09-21 10:18:02 +0200

    add noImg

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './MovieCard.scss';
 
+import noImg from '../../assets/noImg.jpg';
+
 const MovieCard = ({ title, imgPath }) => {
-	return (
-		<div className="card">
-			<img src={`https://image.tmdb.org/t/p/w500${imgPath}`} alt={title} className="card__img" />
-			<p className="card__title">{title}</p>
-		</div>
-	);
+  console.log(imgPath);
+  return (
+    <div className="card">
+      <img
+        src={imgPath === null ? noImg : `https://image.tmdb.org/t/p/w500${imgPath}`}
+        alt={title}
+        className="card__img"
+      />
+      <p className="card__title">{title}</p>
+    </div>
+  );
 };
 
 export default MovieCard;
