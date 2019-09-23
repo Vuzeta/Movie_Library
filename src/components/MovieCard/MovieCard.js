37b15ac41ae56c57fab853a68bec899d1e9ce710
Vuzeta95 <machnio95@o2.pl>
2019-09-23 21:18:59 +0200

    fixed Action.js and MovieCard.js

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -3,17 +3,17 @@ import './MovieCard.scss';
 
 import noImg from '../../assets/noImg.jpg';
 
-const MovieCard = ({ id, title, imgPath, handleMovieID }) => {
-	return (
-		<div className="card" onClick={e => handleMovieID(id)}>
-			<img
-				src={imgPath === null ? noImg : `https://image.tmdb.org/t/p/w500${imgPath}`}
-				alt={title}
-				className="card__img"
-			/>
-			<p className="card__title">{title}</p>
-		</div>
-	);
+const MovieCard = ({ id, title, imgPath }) => {
+  return (
+    <div className="card">
+      <img
+        src={imgPath === null ? noImg : `https://image.tmdb.org/t/p/w500${imgPath}`}
+        alt={title}
+        className="card__img"
+      />
+      <p className="card__title">{title}</p>
+    </div>
+  );
 };
 
 export default MovieCard;
