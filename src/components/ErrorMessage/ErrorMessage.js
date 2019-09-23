6a73d90e568a7805229f2ee14714b7c347ada02d
Vuzeta95 <machnio95@o2.pl>
2019-09-23 20:52:07 +0200

    fixed animations.js, music.js

--- src/components/ErrorMessage/ErrorMessage.js
+++ src/components/ErrorMessage/ErrorMessage.js
@@ -4,12 +4,12 @@ import './ErrorMessage.scss';
 import errorImg from '../../assets/errorImg.png';
 
 const ErrorMessage = () => {
-	return (
-		<div className="errorContainer">
-			<img src={errorImg} alt="error image" className="errorContainer__img" />
-			<h1 className="errorContainer__text">Something went wrong, try again later</h1>
-		</div>
-	);
+  return (
+    <div className="errorContainer">
+      <img src={errorImg} alt="" className="errorContainer__img" />
+      <h1 className="errorContainer__text">Something went wrong, try again later</h1>
+    </div>
+  );
 };
 
 export default ErrorMessage;
--- src/pages/Music/Music.js
+++ src/pages/Music/Music.js
@@ -12,42 +12,71 @@ const _GENDRES_ID = 10402;
 const _LANGUAGE = 'pl-PL';
 
 class Music extends Component {
-	state = {
-		movies: [],
-		total_pages: 0,
-		current_page: 1,
-	};
+  state = {
+    movies: [],
+    total_pages: 0,
+    current_page: 1,
+    error: false,
+  };
 
-	componentDidMount() {
-		const requestHost = `https://api.themoviedb.org/3/discover/movie?api_key=${_APIKEY}&language=${_LANGUAGE}&sort_by=popularity.desc&page=1&with_genres=${_GENDRES_ID}`;
+  changePage = e => {
+    const page = e.selected + 1;
 
-		axios.get(requestHost).then(res => {
-			this.setState({
-				movies: res.data.results,
-				total_pages: res.data.total_pages,
-			});
-		});
-	}
-	render() {
-		const { movies, total_pages } = this.state;
+    const requestHost = `https://api.themoviedb.org/3/discover/movie?api_key=${_APIKEY}&language=${_LANGUAGE}&sort_by=popularity.desc&page=${page}&with_genres=${_GENDRES_ID}`;
 
-		const moviesList = movies.map(card => (
-			<MovieCard
-				key={card.id}
-				id={card.id}
-				title={card.title || card.name}
-				imgPath={card.poster_path}
-			/>
-		));
-		return (
-			<>
-				<h1 className="centerNav__title">{_PAGETITLE}</h1>
-				<div className="movie"></div>
-				<div className="movie">{moviesList}</div>
-				<Pagination total_pages={total_pages} />
-			</>
-		);
-	}
+    axios
+      .get(requestHost)
+      .then(res => {
+        this.setState({
+          movies: res.data.results,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState(prevState => ({
+          error: !prevState.error,
+        }));
+      });
+  };
+
+  componentDidMount() {
+    const requestHost = `https://api.themoviedb.org/3/discover/movie?api_key=${_APIKEY}&language=${_LANGUAGE}&sort_by=popularity.desc&page=1&with_genres=${_GENDRES_ID}`;
+
+    axios
+      .get(requestHost)
+      .then(res => {
+        this.setState({
+          movies: res.data.results,
+          total_pages: res.data.total_pages,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState(prevState => ({
+          error: !prevState.error,
+        }));
+      });
+  }
+  render() {
+    const { movies, total_pages, error } = this.state;
+
+    const moviesList = movies.map(card => (
+      <MovieCard
+        key={card.id}
+        id={card.id}
+        title={card.title || card.name}
+        imgPath={card.poster_path}
+      />
+    ));
+    return (
+      <>
+        <h1 className="centerNav__title">{_PAGETITLE}</h1>
+        <div className="movie"></div>
+        {error ? <ErrorMessage /> : <div className="movie">{moviesList}</div>}
+        {error ? null : <Pagination total_pages={total_pages} changePage={this.changePage} />}
+      </>
+    );
+  }
 }
 
 export default Music;
