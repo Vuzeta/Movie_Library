45bce15797da1e235bde0deddab1c74f5c6be71c
Vuzeta95 <machnio95@o2.pl>
2019-09-30 23:01:10 +0200

    add emoji

--- src/components/Actor/Actor.js
+++ src/components/Actor/Actor.js
@@ -5,8 +5,8 @@ const Actor = ({ i, image, character, name }) => {
   return (
     <div className="actor" key={i}>
       <img src={image} alt="actor poster" className="actor__img" />
-      <p className="actor__name">{`🎭 ${character}`}</p>
-      <p className="actor__name">{name}</p>
+      <p className="actor__name">{`🎥 ${character}`}</p>
+      <p className="actor__name">{`🎭 ${name}`}</p>
     </div>
   );
 };
